caee40ab4897a375514e658546ec32aa842d9e01
nehasingh11 <nehasingh.nitm@gmail.com>
2023-09-16 17:26:17 -0400

    updated

--- Azure-Linux-Virtual-Machine-Jenkins-installed/app-scripts/cloud-init-jenkins.sh
+++ Azure-Linux-Virtual-Machine-Jenkins-installed/app-scripts/cloud-init-jenkins.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install -y openjdk-17-jdk 
 sudo apt install -y openjdk-17-jre 
-sudo apt install git -y
+sudo apt-get install git-all -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo 'deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/' | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update && sudo apt-get install jenkins -y
